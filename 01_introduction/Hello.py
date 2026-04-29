9fd65e2dd750527bce300fee006b2c3ba72f0769
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-04-29 11:14:01 +0530

    Added introduction Python files

---
+++ 01_introduction/Hello.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+# this is a simple Python script that prints two lines of text to the console.
---
+++ 01_introduction/app.py
@@ -0,0 +1,17 @@
+## comments
+# This is a single-line comment in Python.
+"""
+    This is a multi-line comment in Python.
+    It can span multiple lines and is often used for documentation.
+"""
+# Store the final exam score in a variable
+final_exam_score = 85
+
+final_exam_score = 85 # This variable holds the score of the final exam. 
+
+print("Hi,Python")
+print('Hello Python')
+#print("Hi")
+print("--------------------")
+print("    LEARN PYTHON    ")
+print("--------------------")
