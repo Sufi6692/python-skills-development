d7d489d37c6fc66cc5c813e840c7c3356d5f0a45
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-02 18:32:39 +0530

    Build in functions types()

---
+++ 02_strings/Build-In-Function-Type.py
@@ -0,0 +1,11 @@
+# Types
+name = "Baraa"
+print(type(name))
+
+age = 30
+print(type(age))
+print("Your Age is :" + str(age))
+age = 30 + 5
+age = str(age)git st
+print(type(age))
+# age = age + 5 # TypeError: can only concatenate str (not "int") to str
