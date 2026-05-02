8aadd785c02b27f45ff3ccad1f83f8b73e5f93c6
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-02 15:06:18 +0530

    DataTypes

---
+++ 01_introduction/DataTypes.py
@@ -0,0 +1,11 @@
+#Data Types
+a = 10 # a is an integer
+b = 3.14 # b is a float
+c = "Hello" # c is a string
+d = 'A' # d is a character (string of length 1)
+e = "1234" # e is a string, not an integer
+f = True # f is a boolean
+g = False # g is a boolean
+h = None # h is a special value representing "no value"
+i = "" # i is a str blank string
+j = " " # j is a string containing an empty space
---
+++ 01_introduction/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+#Data Types
+a = 10 # a is an integer
+b = 3.14 # b is a float
+c = "Hello" # c is a string
+d = 'A' # d is a character (string of length 1)
+e = "1234" # e is a string, not an integer
+f = True # f is a boolean
