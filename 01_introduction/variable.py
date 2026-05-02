f96649746ef14048f88825c596f0943367f1e298
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-02 12:38:28 +0530

    Variables -

---
+++ 01_introduction/variable.py
@@ -0,0 +1,38 @@
+print("My name is sufi")
+print("Sufi is learning python")
+print("Sufi want to be a data engineer")
+
+
+name = "Qalandar"
+print("My name is", name)
+print(name," is learning python")
+print(name," want to be a data engineer")
+
+name = "Sufi"
+print("My name is", name)
+print(name," is learning python")
+print(name," want to be a data engineer")
+
+name = "Sufi khalandar"
+language = "Python"
+print("My name is", name)
+print(name,"is learning", language)
+print(name,"wants to be become", language, "developer")
+print(name,"wants to be a data engineer")
+
+# Pthon Challenge 
+"""
+ - Print the following three lines .
+ - Add a variable to make it dynamic.
+
+        info@datawithbaraa.com
+        support@datawithbarra.com
+        www.datawithbaraa.com
+ """
+
+User_email = "datawithbaraa.com"
+print("info@"+User_email)
+print("support@"+User_email)
+print("www."+User_email)
+
+
