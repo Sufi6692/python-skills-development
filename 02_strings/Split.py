9e2f021939bd3172831200d6370bf3426e6f1d13
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-03 00:33:51 +0530

    String Repetition

---
+++ 02_strings/Split.py
@@ -0,0 +1,17 @@
+# Transformations
+import csv
+
+
+stamp = "2024-06-01 12:00:00"
+# Split a string into a list of substrings based on a delimiter
+date, time = stamp.split(" ")
+print("Date:", date)
+print("Time:", time)
+
+year,month,day = date.split("-")
+print("Year:", year)
+print("Month:", month)
+print("Day:", day)
+
+csv.file = "1234,Max,USA,1970-10-05,M"
+print(csv.file.split(","))
