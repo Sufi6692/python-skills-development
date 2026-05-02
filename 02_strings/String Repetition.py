dcd43cc4adb122dab778f3bb8a0a6f375af99248
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-03 00:39:52 +0530

    String Repetition

---
+++ 02_strings/String Repetition.py
@@ -0,0 +1,5 @@
+# Transformations
+
+print("Hello " * 3)  # Repeats the string "Hello " three times
+print("abc" * 5)    # Repeats the string "abc" five times
+print("-" * 10)     # Creates a string of 10 dashes
