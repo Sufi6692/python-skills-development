30a3dfec9afa03b04b21ce91d9c2a85d8043e708
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-03 00:12:52 +0530

    F-String

---
+++ 02_strings/Data Transformation f-string.py
@@ -0,0 +1,16 @@
+#f-string
+name = "Alice"
+age = 30
+is_student = False
+print("My name is " + name +", I am " + str(age) + " years old, and I am a student: " + str(is_student) + ".")
+print(f"My name is {name}, I am {age} years old, and I am a student: {is_student}.")
+
+
+print(f"2 + 3 = {2 + 3}")
+print(f"{{This is me}}")#To include literal curly braces in an f-string, you can double them. In this case, `{{` and `}}` will be rendered as `{` and `}` in the output.
+
+
+
+
+
+
