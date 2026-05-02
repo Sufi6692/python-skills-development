f39def7f2809ecb65f5c0dd21f612c3065b294e9
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-02 18:49:44 +0530

    Strings Functions Math

---
+++ 02_strings/String-Function-Math.py
@@ -0,0 +1,16 @@
+# Math
+password = "1234567890"
+print(len(password))
+
+if(len(password) < 8) :
+    print("Password is too short!")
+else:
+    print("Password is long enough.")
+
+
+    text = """
+    python is easy to learn.
+    python is powerful.
+    Many people love Python.
+    """
+print("Python appears " + str(text.count("python"))) 
---
+++ 02_strings/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+    text = """
+    python is easy to learn.
+    python is powerful.
+    Many people love Python.
+    """
+print(text.count("python"))
