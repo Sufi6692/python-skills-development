85539b18a7888e26e5e66d6063749a4d1b493346
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-02 19:13:24 +0530

    Concatinates

---
+++ 02_strings/Data Transformation Join Strings.py
@@ -0,0 +1,9 @@
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+folder = "C:\\Users\\JohnDoe\\Documents"
+file = "report.txt"
+full_path = folder + "\\" + file
+print(full_path)
