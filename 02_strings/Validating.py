de13bb2b90aefdd1904334235f28c8a988cd71ad
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-03 18:44:48 +0530

    Validation

---
+++ 02_strings/Validating.py
@@ -0,0 +1,9 @@
+#validation
+country = "USA"
+print(country.isalpha())
+postal_code = "12345"
+phone = "+1-800-555-1234"
+print(phone.isnumeric())
+print(postal_code.isdigit())
+username = "user_123"
+print(username.isalnum())
