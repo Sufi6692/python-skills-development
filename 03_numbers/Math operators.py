382a0567377f90959cd56d4545cb7b9abd1f5421
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-05 17:13:35 +0530

    Math Operators

---
+++ 03_numbers/Math operators.py
@@ -0,0 +1,29 @@
+# Math operators
+# Addition
+print(2 + 3)  # Output: 5
+# Subtraction
+print(5 - 2)  # Output: 3
+# Multiplication
+print(4 * 3)  # Output: 12
+# Division
+print(10 / 2)  # Output: 5.0
+# Floor Division
+print(10 // 3)  # Output: 3
+# Modulus
+print(10 % 3)  # Output: 1
+# Exponentiation
+print(2 ** 3)  # Output: 8 # 2*2*2
+
+x = 2
+x = x + 3
+print(x) # Output: 5
+
+x = 2
+x += 3
+print(x) # Output: 5
+x *= 2
+print(x) # Output: 10
+x /= 2
+print(x) # Output: 5.0
+x -= 1
+print(x) # Output: 4.0
