bd462f4c5ed40404ee5fc1638d980a0e0f53d246
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-05 17:26:37 +0530

    Rounding

---
+++ 03_numbers/Rounding.py
@@ -0,0 +1,19 @@
+import math
+# Measure Distance
+print(abs(8-5)) # Output: 3
+# Rounding
+print(round(3.14159, 2)) # Output: 3.14
+print(round(3.14159)) # Output: 3
+print(round(2.5)) # Output: 2.0
+print(round(3.5)) # Output: 4.0
+# Floor and Ceil
+print(math.floor(3.7)) # Output: 3
+print(math.ceil(3.2)) # Output: 4
+
+print(math.floor(-3.7)) # Output: -4
+print(math.ceil(-3.2)) # Output: -3
+print(math.trunc(-3.7)) # Output: -3
+print(int(-3.7)) # Output: -3
+
+
+
