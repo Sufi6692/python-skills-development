88faf0e4974c3586996d190741af57c2b24c3c4f
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-05 15:18:25 +0530

    Series using Dictionary

--- Pandas/Panda.py
+++ Pandas/Panda.py
@@ -49,3 +49,20 @@ s["apple":"grapes":2] #start(inclusive) and end(inclusive): step value (Value by
 print("Slicing with new index and step:")
 print(s["apple":"grapes":2])
 
+fruit_protein ={
+    "Avocado" :2.0,
+    "Guava" : 2.6,
+    "Blackberries" : 2.0,
+    "Oranges" : 0.9,
+    "Banana" :1.1,
+    "Apples" : 0.3,
+    "Kiwi" : 1.1,
+    "Pomegranate" : 1.7,
+    "Mango" : 0.8,
+    "Cherries" : 1.0
+
+ }
+print("Fruit Protein Dictionary:")
+print(fruit_protein)
+s2 = pd.Series(fruit_protein, name="Protein")
+print(s2)
