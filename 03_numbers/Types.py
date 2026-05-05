6a36f523a927f7209d12f445ab5704f79612fff8
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-05 17:08:37 +0530

    Types in Numeric

---
+++ 03_numbers/Types.py
@@ -0,0 +1,34 @@
+x = 5
+y = 5.7
+z = 1 + 2j
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = "24"
+print(type(x))
+x = int(x)
+print(type(x))
+print(x + 10)
+
+x = 3.14
+y = int(x)
+print(type(x))
+print(type(y))
+print(y)
+
+x = 3
+y = float(x)
+print(type(x))
+print(type(y))
+print(y)
+
+x = "3.14"
+y = float(x)
+print(type(x))
+print(type(y))
+print(y)
+
+x  = 3
+y =  4
+print(complex(x, y))
