ab66073f27269d95fcf23a04428ff1cf1d20bdd6
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-05-05 17:38:30 +0530

    Validation

---
+++ 03_numbers/Validation.py
@@ -0,0 +1,14 @@
+import random
+x = 7.0
+print(x.is_integer)
+
+
+y = 40.2
+print(y.is_integer)
+
+x = 40.2
+print(isinstance(x,int))
+print(isinstance(x,float))
+
+x = print(random.randint(1,100))
+
